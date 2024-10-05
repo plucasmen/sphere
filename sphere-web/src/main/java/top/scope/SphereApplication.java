7cd4973a70ce486e8e0c8956525912f32e6fc525
ysir <1653870146@qq.com>
2024-10-05 18:49:54 +0800

    chore(docs): 多模块初始化

---
+++ sphere-web/src/main/java/top/scope/SphereApplication.java
@@ -0,0 +1,13 @@
+package top.scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SphereApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SphereApplication.class, args);
+    }
+}
